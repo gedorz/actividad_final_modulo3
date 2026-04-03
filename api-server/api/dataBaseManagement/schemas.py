6a425915cc1d9cf642a06517262ca5d1b56794a1
GERMAN DARIO REALPE ZAMBRANO <gedorz@gmail.com>
2026-04-03 23:35:29 +0200

    Api estable

--- api-server/api/dataBaseManagement/schemas.py
+++ api-server/api/dataBaseManagement/schemas.py
@@ -1,18 +1,21 @@
 from datetime import datetime, date
 from pydantic import BaseModel, Field   
 
-# Is done: Modelos Pydantic
+# Is done: Modelos Pydantic para la gestión de tareas
+# Modelo para crear una tarea de forma simplificada, sin campos de ID o timestamps
 class TaskCreate(BaseModel):
     titulo: str = Field(min_length=1, max_length=100, description="Título de la tarea")
     contenido: str = Field(min_length=1, max_length=200, description="Contenido de la tarea")
     deadline: date = Field(description="Fecha de vencimiento")
 
+# Modelo para actualizar una tarea, permitiendo editar todos los campos excepto el ID y los timestamps
 class TaskUpdate(BaseModel):
-    titulo: str = Field(min_length=1, max_length=100, description="Edit Título de la tarea") 
-    contenido: str = Field(min_length=1, max_length=200, description="Edit Contenido de la tarea")
-    deadline: date = Field(description="Edit fecha de vencimiento")
-    completada: bool = Field(description="Edit estado de completado")
+    titulo: str = Field(min_length=1, max_length=100, description="Edita título de la tarea") 
+    contenido: str = Field(min_length=1, max_length=200, description="Edita contenido de la tarea")
+    deadline: date = Field(description="Edita fecha de vencimiento")
+    completada: bool = Field(description="Edita estado de completado")
 
+# Modelo para la respuesta de la API, incluyendo todos los campos de la tarea
 class TaskResponse(BaseModel):
     id: int
     titulo: str
--- api-server/api/main.py
+++ api-server/api/main.py
@@ -4,4 +4,9 @@ app = init_fastapi()
 app.include_router(router)
 
 
+@app.get("/api/openapi.json", include_in_schema=False)
+def openapi_proxy_alias():
+	return app.openapi()
+
+
 
